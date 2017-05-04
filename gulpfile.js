2e39dd8492efa5839c54ef52ba924791618a3efd
Hamza El Ouakili <Hamza El Ouakili>
2017-05-04 15:27:43 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+});
+
+
+gulp.task('sass', function(){
+  return gulp.src('app/sass/**/*.sass')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function(){
+  gulp.watch('app/sass/**/*.sass', ['sass']); 
+  // Other watchers
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload); 
+
+});
